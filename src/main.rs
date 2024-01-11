7f27b818ed4ea8e7182308f6ba9148030ebe2577
loeeeee <95266635+loeeeee@users.noreply.github.com>
2024-01-11 08:37:18 +0000

    load env var

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 mod series;
+
 use log::debug;
 use log::error;
 use log::info;
 use log::warn;
 use dotenv::dotenv;
+
+fn load_env_var(var_name: &str) -> Result<String, std::env::VarError> {
+    debug!("Loading env var: {}", &var_name);
+    match std::env::var(&var_name) {
+        Ok(var_value) => {
+            info!("{}={}", &var_name, &var_value);
+            return Ok(var_value)
+        },
+        Err(e) => {
+            error!("Failed to load TARGET_DIR env var. Please check .env file.");
+            return Err(e)
+        },
+    };
+}
 
 fn main() {
     // Load .env file
@@ -13,21 +28,13 @@ fn main() {
     env_logger::init();
 
     // TODO: Check source directory
+    let source_directory = load_env_var(&"SOURCE_DIR").unwrap();
 
     // Check and create target directory
     debug!("Checking and creating folder for target mapping.");
-    let target_directory = match std::env::var("TARGET_DIR") {
-        Ok(target_dir) => {
-            info!("TARGET_DIR={}", target_dir);
-            target_dir
-        },
-        Err(_) => {
-            error!("Failed to load TARGET_DIR env var. Please check .env file.");
-            panic!()
-        },
-    };
+    let target_directory = load_env_var(&"TARGET_DIR").unwrap();
     series::create_folder(&target_directory);
-    info!("Finish creating folder {} for target mapping.", target_directory);
+    debug!("Finish creating folder {} for target mapping.", target_directory);
 
     // Iterate through source directory to resolve each anime series
     
