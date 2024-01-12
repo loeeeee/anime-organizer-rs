5a55a7baf176ed1f1b943c5e3dbb48dbd6972781
loeeeee <95266635+loeeeee@users.noreply.github.com>
2024-01-12 13:34:22 +0000

    test framework

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,26 @@
 #[cfg(test)]
 mod tests {
-    todo!();
+    use std::fs;
+
+    use anime_organizer_rs::load_env_var;
+    use log::{warn, error};
+    use crate::series::Series;
+
+    #[test]
+    fn series_name_extraction() {
+        dotenvy::from_filename("test.env").unwrap();
+
+        let source_dir = load_env_var("SOURCE_DIR").unwrap();
+        println!("{}", &source_dir);
+
+        let paths = match fs::read_dir(&source_dir) {
+            Ok(paths) => paths,
+            Err(e) => {
+                error!("Failed open {}, due to {}", &source_dir, &e);
+                panic!();
+            }
+        };
+
+        let series = Series::new(&source_dir);
+    }
 }
