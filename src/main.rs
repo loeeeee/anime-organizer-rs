1d7ec3ff81687279616c1b88c1d9aa659cbe0f76
loeeeee <95266635+loeeeee@users.noreply.github.com>
2024-01-12 07:10:34 +0000

    implement filterwords

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 mod series;
 
+use std::fs::File;
+use std::io::Read;
+
+use env_logger::filter::Filter;
 use log::debug;
 use log::error;
 use log::info;
 use log::warn;
 use dotenv::dotenv;
+use serde::Deserialize;
+
 
 fn load_env_var(var_name: &str) -> Result<String, std::env::VarError> {
     debug!("Loading env var: {}", &var_name);
@@ -18,6 +24,36 @@ fn load_env_var(var_name: &str) -> Result<String, std::env::VarError> {
             return Err(e)
         },
     };
+}
+
+#[derive(Debug, Deserialize)]
+struct FilterWords {
+    cc_group: Vec<String>,
+    meta_tag: Vec<String>,
+    low_priority: Vec<String>,
+}
+
+impl FilterWords {
+    fn load() -> FilterWords {
+        let filter_words_file_path = "./static/filter_words.yaml";
+        let mut filter_words_string = String::new();
+        match File::open(filter_words_file_path).unwrap().read_to_string(&mut filter_words_string) {
+            Ok(_) => info!("Load filter words successfully."),
+            Err(_) => error!("Failed to load filter words, please check {}", &filter_words_file_path),
+        };
+    
+        // Parse the YAML content into the ServerConfig struct
+        match serde_yaml::from_str(&filter_words_string) {
+            Ok(content) => {
+                info!("Parse filter words successfully.");
+                content
+            },
+            Err(err) => {
+                error!("Error parsing YAML: {}", err);
+                panic!();
+            }
+        }
+    }
 }
 
 fn main() {
--- src/series.rs
+++ src/series.rs
@@ -2,6 +2,9 @@ use std::fs;
 use log::error;
 use log::warn;
 use log::info;
+use regex::Regex;
+
+use crate::FilterWords;
 
 pub fn create_folder(full_path: &str) {
     if !fs::metadata(&full_path).is_ok() {
@@ -10,4 +13,53 @@ pub fn create_folder(full_path: &str) {
     } else {
         info!("Target folder already exists. Skipping the creation.")
     }
+}
+
+struct Series {
+    name: String,
+    location: String,
+    seasons: Vec<Season>,
+}
+
+struct Season {
+    sequence: i8,
+    location: String,
+    episodes: Vec<Episode>,
+}
+
+struct Episode {
+    sequence: i16,
+    location: String,
+    subtitles: Vec<Subtitle>,
+}
+
+struct Subtitle {
+    language: String,
+    location: String,
+}
+
+impl Series {
+
+    fn extract_series_name(&self, folder_name: &str, filter_words: &FilterWords) -> Result<String, ()> {
+        /// # Return the cleaned series name inferred from folder name
+    
+        // Remove CC group name
+        let filter_construct_middleware: Vec<String> = FilterWords::cc_group.iter()
+        .map(|i| format!("({})", i))
+        .collect();
+    
+        let combined = filter_construct_middleware.join("|").replace(".", ".").replace("-", "-");
+        let reg_str = format!(r"{}(&{})*?", combined, combined);
+        let reg = Regex::new(&reg_str).expect("Invalid regex pattern");
+    
+    
+    
+        // Remove meta tags
+        todo!() 
+    }
+}
+
+
+fn extract_episode_number(file_name: &str) -> Result<String, ()> {
+    todo!()
 }
